9441e39ef0acea2ff9681a8b74c41b40e6639608
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-10-18 15:47:44 -0300

    arrumando urls arrumando a maioria dos templates... alguns templates não estão herdando do template base... isso tem que ser arrumado... refs #17 #16

--- esporte_tec/urls.py
+++ esporte_tec/urls.py
@@ -9,6 +9,9 @@ urlpatterns = patterns('',
 
      url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
      url(r'^membros/$', TemplateView.as_view(template_name="membros.html"), name='membros'),
+     url(r'^contato/$', TemplateView.as_view(template_name="contato.html"), name='contato'),
+     url(r'^noticias/$', TemplateView.as_view(template_name="noticias.html"), name='noticias'),
+     url(r'^sobre/$', TemplateView.as_view(template_name="sobre.html"), name='sobre'),
     # Examples:
     # url(r'^$', 'esporte_tec.views.home', name='home'),
     # url(r'^esporte_tec/', include('esporte_tec.foo.urls')),
