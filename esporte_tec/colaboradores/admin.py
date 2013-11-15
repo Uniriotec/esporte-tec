872c9d85bcff6a6520bb62c3d7214900062d252b
tainacleal <tainacleal@gmail.com>
2013-11-15 10:56:00 -0200

    Issue #48 Criado model de Academia

--- esporte_tec/colaboradores/admin.py
+++ esporte_tec/colaboradores/admin.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from esporte_tec.colaboradores.models import Colaborador
 from esporte_tec.colaboradores.models import Governo
+from esporte_tec.colaboradores.models import Academia
 
 admin.site.register(Colaborador, admin.ModelAdmin)
 admin.site.register(Governo, admin.ModelAdmin)
+admin.site.register(Academia, admin.ModelAdmin)
--- esporte_tec/colaboradores/models.py
+++ esporte_tec/colaboradores/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
+
+
 class Colaborador(models.Model):
 
 	"""
@@ -26,8 +28,24 @@ class Governo(models.Model):
 	departamento_governo = models.CharField("Departamento", max_length=250)
 	email_governo = models.CharField("Email", max_length = 250)
 	area_interesse_governo = models.CharField("Área de interesse", max_length=250)
-	telefone_governo = models.CharField("Telefone", max_length=20)
+	telefone_governo = models.CharField("Telefone", max_length=40)
 
 	def __unicode__(self):
 		return "%s - %s - %s - %s - %s - %s" % (self.razao_social_governo, self.contato_governo, self.departamento_governo, self.email_governo, self.area_interesse_governo, self.telefone_governo)
+
+class Academia(models.Model):
+	"""
+	Model de cadastro de Academia
+	"""
+
+	razao_social_academia = models.CharField("Razão Social", max_length=250)
+	contato_academia = models.CharField("Pessoa para contato", max_length=250)
+	departamento_academia = models.CharField("Departamento", max_length=250)
+	email_academia = models.CharField("Email", max_length=250)
+	telefone_academia = models.CharField("Telefone", max_length=40)
+	area_pesquisa = models.CharField("Área de pesquisa", max_length=250)
+	area_interesse_academia = models.CharField("Área de interesse", max_length=250)
+
+	def __unicode__(self):
+		return "%s - %s - %s - %s - %s - %s - %s" % (self.razao_social_academia, self.contato_academia, self.departamento_academia, self.email_academia, self.telefone_academia, self.area_pesquisa, self.area_interesse_academia)
 
