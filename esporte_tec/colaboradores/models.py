3361d15b798dc52c81106bd4f82ee4d37b15d86f
tainacleal <tainacleal@gmail.com>
2013-11-15 19:54:51 -0200

    Aperfeiçoando models Governo e Academia Fix #47 Fix #48

--- esporte_tec/colaboradores/models.py
+++ esporte_tec/colaboradores/models.py
@@ -25,31 +25,39 @@ class OrgaoGoverno(models.Model):
 	Model de cadastro de Governo
 	"""
 
-	razao_social_governo = models.CharField("Razão Social", max_length=250)
-	contato_governo = models.CharField("Pessoa para contato", max_length=250)
-	departamento_governo = models.CharField("Departamento", max_length=250)
-	email_governo = models.CharField("Email", max_length = 250)
-	area_interesse_governo = models.CharField("Área de interesse", max_length=250)
-	telefone_governo = models.CharField("Telefone", max_length=40)
+	razao_social = models.CharField("Razão Social", max_length=250)
+	contato = models.CharField("Pessoa para contato", max_length=250)
+	departamento = models.CharField("Departamento", max_length=250)
+	email = models.EmailField("Email", max_length = 250)
+	area_interesse = models.CharField("Área de interesse", max_length=250)
+	telefone = models.CharField("Telefone", max_length=40)
+
+	class Meta:
+		verbose_name_plural = "Orgãos Governamentais"
+		verbose_name = "Orgão Governamental"
 
 	def __unicode__(self):
-		return "%s - %s - %s - %s - %s - %s" % (self.razao_social_governo, self.contato_governo, self.departamento_governo, self.email_governo, self.area_interesse_governo, self.telefone_governo)
+		return "%s - %s - %s - %s - %s - %s" % (self.razao_social, self.contato, self.departamento, self.email, self.area_interesse, self.telefone)
 
 class Academia(models.Model):
 	"""
 	Model de cadastro de Academia
 	"""
 
-	razao_social_academia = models.CharField("Razão Social", max_length=250)
-	contato_academia = models.CharField("Pessoa para contato", max_length=250)
-	departamento_academia = models.CharField("Departamento", max_length=250)
-	email_academia = models.CharField("Email", max_length=250)
-	telefone_academia = models.CharField("Telefone", max_length=40)
+	razao_social = models.CharField("Razão Social", max_length=250)
+	contato = models.CharField("Pessoa para contato", max_length=250)
+	departamento = models.CharField("Departamento", max_length=250)
+	email = models.EmailField("Email", max_length=250)
+	telefone = models.CharField("Telefone", max_length=40)
 	area_pesquisa = models.CharField("Área de pesquisa", max_length=250)
-	area_interesse_academia = models.CharField("Área de interesse", max_length=250)
+	area_interesse = models.CharField("Área de interesse", max_length=250)
+
+	class Meta:
+		verbose_name_plural = "Instituições Acadêmicas"
+		verbose_name = "Instituição Acadêmica"
 
 	def __unicode__(self):
-		return "%s - %s - %s - %s - %s - %s - %s" % (self.razao_social_academia, self.contato_academia, self.departamento_academia, self.email_academia, self.telefone_academia, self.area_pesquisa, self.area_interesse_academia)
+		return "%s - %s - %s - %s - %s - %s - %s" % (self.razao_social, self.contato, self.departamento, self.email, self.telefone, self.area_pesquisa, self.area_interesse)
 
 class Startup(models.Model):
     """
