39e94105a3fa6f0078c6b3078ad6b9b387dc8f6f
tainacleal <tainacleal@gmail.com>
2013-11-15 09:42:47 -0200

    Issue #47 Criando model de Governo

--- esporte_tec/colaboradores/admin.py
+++ esporte_tec/colaboradores/admin.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from esporte_tec.colaboradores.models import Colaborador
+from esporte_tec.colaboradores.models import Governo
 
 admin.site.register(Colaborador, admin.ModelAdmin)
+admin.site.register(Governo, admin.ModelAdmin)
--- esporte_tec/colaboradores/models.py
+++ esporte_tec/colaboradores/models.py
@@ -15,3 +15,19 @@ class Colaborador(models.Model):
 	def __unicode__(self):
 		return "%s - %s - %s" % (self.nome_colaborador, self.email_colaborador, self.area_interesse)
 
+
+class Governo(models.Model):
+	"""
+	Model de cadastro de Governo
+	"""
+
+	razao_social_governo = models.CharField("Razão Social", max_length=250)
+	contato_governo = models.CharField("Pessoa para contato", max_length=250)
+	departamento_governo = models.CharField("Departamento", max_length=250)
+	email_governo = models.CharField("Email", max_length = 250)
+	area_interesse_governo = models.CharField("Área de interesse", max_length=250)
+	telefone_governo = models.CharField("Telefone", max_length=20)
+
+	def __unicode__(self):
+		return "%s - %s - %s - %s - %s - %s" % (self.razao_social_governo, self.contato_governo, self.departamento_governo, self.email_governo, self.area_interesse_governo, self.telefone_governo)
+
