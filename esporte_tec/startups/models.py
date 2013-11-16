1b8dc1439f8b2ff62aeb84283f638d44a151ccd3
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-11-16 19:29:59 -0200

    colocando ligação de area de interesse com startup refs #4 #52

--- esporte_tec/startups/models.py
+++ esporte_tec/startups/models.py
@@ -5,7 +5,7 @@ class Startup(models.Model):
     """
     Model para o cadastro de Startups
     Area inovação e com usted Arruda
-    """	
+    """
 
     razao_social = models.CharField("Razão Social", max_length=250)
     nome_fantasia = models.CharField("Nome fantasia", max_length=250)
@@ -14,10 +14,11 @@ class Startup(models.Model):
     telefone = models.CharField("Telefone", max_length=40)
     nome = models.CharField("Nome", max_length=40)
     pessoa_para_contato = models.CharField("Pessoa para contato",max_length=250)
+    area_interesse = models.ManyToManyField('areas.Area', verbose_name="Área de Interesse")
 
     class Meta:
-    	verbose_name_plural = "Startups"
-    	verbose_name = "Startup"
+        verbose_name_plural = "Startups"
+        verbose_name = "Startup"
 
     def __unicode__(self):
         return "%s - %s - %s - %s - %s -%s - %s" % (self.razao_social, self.nome_fantasia, self.site, self.email, self.telefone, self.nome, self.pessoa_para_contato)
