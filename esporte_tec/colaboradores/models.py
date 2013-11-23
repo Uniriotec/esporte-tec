35e7b734c19cf4c79bbbf53b32abee8cb82c359e
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-11-23 16:52:31 -0200

    colocando campo de area de pesquisa em instituição academica, instalado outra app para isso. Não esqueçam de instalar os novos requirements. fix #58

--- esporte_tec/colaboradores/models.py
+++ esporte_tec/colaboradores/models.py
@@ -1,65 +1,71 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
+from model_utils import Choices
+
 
 
 class Colaborador(models.Model):
 
-	"""
-	Model responsável pelos Colaboradores
-	"""
+    """
+    Model responsável pelos Colaboradores
+    """
 
-	nome = models.CharField("Nome", max_length=250)
-	email = models.EmailField("Email", max_length=250)
-	area_interesse = models.ManyToManyField('areas.Area', verbose_name="Área de Interesse")
+    nome = models.CharField("Nome", max_length=250)
+    email = models.EmailField("Email", max_length=250)
+    area_interesse = models.ManyToManyField('areas.Area', verbose_name="Área de Interesse")
 
 
-	class Meta:
-		verbose_name_plural = "Colaboradores"
+    class Meta:
+        verbose_name_plural = "Colaboradores"
 
-	def __unicode__(self):
-		return "%s - %s" % (self.nome, self.email)
+    def __unicode__(self):
+        return "%s - %s" % (self.nome, self.email)
 
 
 class OrgaoGoverno(models.Model):
-	"""
-	Model de cadastro de Governo
-	"""
+    """
+    Model de cadastro de Governo
+    """
 
-	razao_social = models.CharField("Razão Social", max_length=250)
-	contato = models.CharField("Pessoa para contato", max_length=250)
-	departamento = models.CharField("Departamento", max_length=250)
-	email = models.EmailField("Email", max_length = 250)
-	area_interesse = models.ManyToManyField('areas.Area', verbose_name="Área de Interesse")
-	telefone = models.CharField("Telefone", max_length=40)
+    razao_social = models.CharField("Razão Social", max_length=250)
+    contato = models.CharField("Pessoa para contato", max_length=250)
+    departamento = models.CharField("Departamento", max_length=250)
+    email = models.EmailField("Email", max_length = 250)
+    area_interesse = models.ManyToManyField('areas.Area', verbose_name="Área de Interesse")
+    telefone = models.CharField("Telefone", max_length=40)
 
-	class Meta:
-		verbose_name_plural = "Orgãos Governamentais"
-		verbose_name = "Orgão Governamental"
+    class Meta:
+        verbose_name_plural = "Orgãos Governamentais"
+        verbose_name = "Orgão Governamental"
 
-	def __unicode__(self):
-		return "%s - %s - %s - %s - %s" % (self.razao_social, self.contato, self.departamento, self.email, self.telefone)
+    def __unicode__(self):
+        return "%s - %s - %s - %s - %s" % (self.razao_social, self.contato, self.departamento, self.email, self.telefone)
 
 
 class Academia(models.Model):
-	"""
-	Model de cadastro de Academia
-	"""
+    """
+    Model de cadastro de Instituições Acadêmicas
+    """
 
-	razao_social = models.CharField("Razão Social", max_length=250)
-	contato = models.CharField("Pessoa para contato", max_length=250)
-	departamento = models.CharField("Departamento", max_length=250)
-	email = models.EmailField("Email", max_length=250)
-	telefone = models.CharField("Telefone", max_length=40)
-	area_interesse = models.ManyToManyField('areas.Area', verbose_name="Área de Interesse")
+    AREA_PESQUISA_CHOICES = Choices(
+        (0, 'engenharia', 'engenharia'),
+        (1, 'c_sociais', 'ciências sociais'),
+        (2, 'c_saude', 'ciências da saúde'),
+    )
 
-	class Meta:
-		verbose_name_plural = "Instituições Acadêmicas"
-		verbose_name = "Instituição Acadêmica"
+    razao_social = models.CharField("Razão Social", max_length=250)
+    contato = models.CharField("Pessoa para contato", max_length=250)
+    departamento = models.CharField("Departamento", max_length=250)
+    email = models.EmailField("Email", max_length=250)
+    telefone = models.CharField("Telefone", max_length=40)
+    area_interesse = models.ManyToManyField('areas.Area', verbose_name="Área de Interesse")
 
-	def __unicode__(self):
-		return "%s - %s - %s - %s - %s" % (self.razao_social, self.contato, self.departamento, self.email, self.telefone)
+    area_pesquisa = models.IntegerField("Área de Pesquisa",choices=AREA_PESQUISA_CHOICES, default=AREA_PESQUISA_CHOICES.engenharia)
 
+    class Meta:
+        verbose_name_plural = "Instituições Acadêmicas"
+        verbose_name = "Instituição Acadêmica"
 
-
-
+    def __unicode__(self):
+        return "%s - %s - %s - %s - %s" % (self.razao_social, self.contato, self.departamento, self.email, self.telefone)
