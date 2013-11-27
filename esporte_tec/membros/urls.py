d9fe439832c5e60f91ba36b11c08abf890895e9d
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-11-27 15:48:41 -0200

    fazendo uma base para view e template de cadastro de empresa refs #5 #6

--- esporte_tec/membros/urls.py
+++ esporte_tec/membros/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from esporte_tec.membros import views
 
 urlpatterns = patterns('',
-    url(r'^cadastro/$', views.cadastrar_empresa, name='cadastro'),
+    url(r'^cadastro_empresa/$', views.cadastrar_empresa, name='cadastro_empresa'),
 )
 
 
--- esporte_tec/membros/views.py
+++ esporte_tec/membros/views.py
@@ -1,8 +1,33 @@
 # -*- coding: utf-8 -*
-from django.shortcuts import render_to_response, get_object_or_404, redirect
-from django.template import RequestContext
+from django.shortcuts import redirect
 
-from esporte_tec.membros.models import Empresa
+from annoying.decorators import render_to
 
+from esporte_tec.membros.forms import EmpresaForm
+
+@render_to("cadastro_empresa.html")
 def cadastrar_empresa(request):
-	empresa 
+
+    if request.method == 'POST':
+        # pega os dados do post e joga no form
+        form = EmpresaForm(request.POST)
+
+        # se for valido salva e redireciona
+        if form.is_valid():
+            form.save()
+            # criar uma pagina de "cadastro realizado com sucesso e redirecionar para ela no lugar da home
+            return redirect('index')
+
+    else:
+        # cria um formulario em branco
+        form = EmpresaForm()
+
+    # se for invalido carrega a mesma pagina mas nesse ponto a variavel form já foi validada e portanto vai
+    # ter os dados dos erros do formulario
+
+    # Se chegou aqui é por que ou foi um GET, e portanto o form está vazio, ou foi
+    # um post mas com erros de validação no form. Assim vai carregar o template indicado, com os dados do
+    # form (vazio se for GET ou form com erros se for invalido)
+    return {
+        'form': form,
+    }
