179399eff7703593a2325c5b777240c1ccaf7d27
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-11-27 16:54:00 -0200

    colocando shelscript como executavel e pondo --confirm esporte-tec no final de um comando para ir automaticamente

--- heroku_cmds.sh
+++ heroku_cmds.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-heroku pg:reset DATABASE_URL --app esporte-tec
+heroku pg:reset DATABASE_URL --app esporte-tec --confirm esporte-tec
 heroku run python manage.py syncdb --noinput --app esporte-tec
 heroku run python manage.py loaddata esporte_tec/fixtures/auth_user.yaml --app esporte-tec
 heroku run python manage.py loaddata esporte_tec/fixtures/sites.yaml --app esporte-tec
