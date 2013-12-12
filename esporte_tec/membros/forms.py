927e67524935e514cfda2de8953d56e82a048f51
Lanchinho <guilherme.albuquerque@uniriotec.br>
2013-12-12 19:23:48 -0200

    Criação e implementação das páginas de cadastro para instituição acadêmica, instituição Governamental e Golaboradores refs #68. Adição da imagem em construção na página membros e criação da página de cadastro!

--- esporte_tec/membros/forms.py
+++ esporte_tec/membros/forms.py
@@ -2,6 +2,9 @@
 from django import forms
 
 from esporte_tec.membros.models import Empresa
+from esporte_tec.membros.models import Colaborador
+from esporte_tec.membros.models import Academia
+from esporte_tec.membros.models import OrgaoGoverno
 
 class EmpresaForm(forms.ModelForm):
     "Form do modelo de Empresa"
@@ -9,4 +12,21 @@ class EmpresaForm(forms.ModelForm):
     class Meta:
         model = Empresa
 
+class ColaboradorForm(forms.ModelForm):
+	"Form do modelo de Colaborador"
+
+	class Meta:
+		model = Colaborador
+
+class AcademiaForm(forms.ModelForm):
+	"Form do modelo de Instituição Academica"
+
+	class Meta:
+		model = Academia
+
+class GovernoForm(forms.ModelForm):
+	"Form do modelo de Instituição Governamental"
+
+	class Meta:
+		model = OrgaoGoverno
 
--- esporte_tec/membros/views.py
+++ esporte_tec/membros/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import redirect
 from annoying.decorators import render_to
 
 from esporte_tec.membros.forms import EmpresaForm
+from esporte_tec.membros.forms import ColaboradorForm
+from esporte_tec.membros.forms import AcademiaForm
+from esporte_tec.membros.forms import GovernoForm
+
 
 @render_to("cadastro_empresa.html")
 def cadastrar_empresa(request):
@@ -29,5 +33,53 @@ def cadastrar_empresa(request):
     # um post mas com erros de validação no form. Assim vai carregar o template indicado, com os dados do
     # form (vazio se for GET ou form com erros se for invalido)
     return {
+        'form': form,
+    }
+
+@render_to("cadastro_colaborador.html")
+def cadastrar_colaborador(request):
+
+    if request.method == 'POST':
+        form = ColaboradorForm(request.POST)
+        form.save()
+
+        return redirect('sucesso')
+
+    else:
+        form = ColaboradorForm()
+
+    return{
+        'form': form,
+    }
+
+@render_to("cadastro_academia.html")
+def cadastrar_instituicao_academica(request):
+
+    if request.method == 'POST':
+        form = AcademiaForm(request.POST)
+        form.save()
+
+        return redirect('sucesso')
+
+    else:
+        form = AcademiaForm()
+
+    return{
+        'form': form,
+    }
+
+@render_to("cadastro_governo.html")
+def cadastrar_instituicao_governamental(request):
+
+    if request.method == 'POST':
+        form = GovernoForm(request.POST)
+        form.save()
+
+        return redirect('sucesso')
+
+    else:
+        form = GovernoForm()
+
+    return{
         'form': form,
     }
