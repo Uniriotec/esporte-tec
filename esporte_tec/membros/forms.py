f61a18562bf46d0ca2b93ba92f642909b489dde1
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-12-21 19:52:52 -0200

    arrumando texto de helper do campo area_interesse em todos os modelForms

--- esporte_tec/membros/forms.py
+++ esporte_tec/membros/forms.py
@@ -6,27 +6,45 @@ from esporte_tec.membros.models import Colaborador
 from esporte_tec.membros.models import Academia
 from esporte_tec.membros.models import OrgaoGoverno
 
+helper_selecionar_mais_opcoes = 'Para selecionar mais de uma opção, mantenha pressionada a tecla "Control" (ou "Command" se estiver utilizando um Mac).'
+
 class EmpresaForm(forms.ModelForm):
     "Form do modelo de Empresa"
+
+    def __init__(self, *args, **kwargs):
+        super(EmpresaForm, self).__init__(*args, **kwargs)
+        self.fields['area_interesse'].help_text = helper_selecionar_mais_opcoes
 
     class Meta:
         model = Empresa
 
 class ColaboradorForm(forms.ModelForm):
-	"Form do modelo de Colaborador"
+    "Form do modelo de Colaborador"
 
-	class Meta:
-		model = Colaborador
+    def __init__(self, *args, **kwargs):
+        super(ColaboradorForm, self).__init__(*args, **kwargs)
+        self.fields['area_interesse'].help_text = helper_selecionar_mais_opcoes
+
+    class Meta:
+        model = Colaborador
 
 class AcademiaForm(forms.ModelForm):
-	"Form do modelo de Instituição Academica"
+    "Form do modelo de Instituição Academica"
 
-	class Meta:
-		model = Academia
+    def __init__(self, *args, **kwargs):
+        super(AcademiaForm, self).__init__(*args, **kwargs)
+        self.fields['area_interesse'].help_text = helper_selecionar_mais_opcoes
+
+    class Meta:
+        model = Academia
 
 class GovernoForm(forms.ModelForm):
-	"Form do modelo de Instituição Governamental"
+    "Form do modelo de Instituição Governamental"
 
-	class Meta:
-		model = OrgaoGoverno
+    def __init__(self, *args, **kwargs):
+        super(GovernoForm, self).__init__(*args, **kwargs)
+        self.fields['area_interesse'].help_text = helper_selecionar_mais_opcoes
+
+    class Meta:
+        model = OrgaoGoverno
 
